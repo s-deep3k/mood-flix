9d35cac6f2e84c48c85051f951ffaed7c4564c18
Shankhadeep Biswas <shankha@Shankhadeeps-MacBook-Air.local>
2025-02-09 03:37:39 +0530

    Fetching Movies with effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Search from './components/Search.jsx';
-
+const API_BASE_URL = 'https://api.themoviedb.org/3';
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const fetchMovies = async () => {
+    try{
+      const endpoint = `${API_BASE_URL}/discover/movie/sort_by=popularity.desc`;
+    }
+    catch(error: Error){
+      setErrorMsg(error?.message);
+    }
+  }
+
+  useEffect(() => {
+      fetchMovies()
+  }, [searchTerm]);
   return (
     <main>
       <div className="pattern"/> 
@@ -11,9 +25,13 @@ const App = () => {
         <header>
           <img src="./hero.png" alt="Hero Banner" />
           <h1>Find <span className="text-gradient">Movies</span> you'll enjoy without the hassle</h1>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         </header>
 
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        <section className='all-movies'>
+          <h2>All Movies</h2>
+          {errorMsg && <p className="text-red-500">{errorMsg}</p>}
+        </section>
       </div>
     </main>
   )
